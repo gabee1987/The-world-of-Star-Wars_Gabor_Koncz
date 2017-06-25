255b74fa119b760209f550f281ea5baf833cc4fe
gabee1987 <gabee1987@gmail.com>
2017-06-25 19:16:04 +0200

    HEroku reversed

--- db_connect.py
+++ db_connect.py
@@ -7,26 +7,28 @@
 import os
 import psycopg2
 import urllib
-'''from local_config import *'''
+from local_config import *
+
+
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 
 def connect_db(connect_data):
     '''
         Set the connection with the database.
     '''
-    connection = None
+    conn = None
     try:
-        urllib.parse.uses_netloc.append('postgres')
-        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        connection = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-        conn = psycopg2.connect(connection)
-        return connection
+        conn = psycopg2.connect(connect_data)
+        return conn
     except Exception as error:
         print(error)
         return 'connection error'
@@ -37,8 +39,8 @@ def handle_database(query):
         Handle querys from database.
     '''
     result = {}
-    '''connect_data = "dbname={0} user={1} password={2} host={3}".format(DATABASE, USER, PASSWORD, HOST)'''
-    connection = connect_db(connection)
+    connect_data = "dbname={0} user={1} password={2} host={3}".format(DATABASE, USER, PASSWORD, HOST)
+    connection = connect_db(connect_data)
     if connection == 'connection error':
         result = 'Connection error. Server unreachable.'
         return result
